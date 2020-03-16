1d7043cfa75671f9c941f6db99e759bffe97fad6
ta93n <nideratakumin@gmail.com>
2020-03-16 20:35:08 +0900

    レイアウト作成とルーティングの設定

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -1,9 +1,19 @@
 require 'test_helper'
 
 class StaticPagesControllerTest < ActionDispatch::IntegrationTest
+
   test "should get home" do
-    get static_pages_home_url
+    get root_path
     assert_response :success
   end
 
+  test "should get about" do
+    get about_path
+    assert_response :success
+  end
+
+  test "should get contact" do
+    get contact_path
+    assert_response :success
+  end
 end
