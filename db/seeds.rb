74361af00db817492cf8f36e09b8901d98f7017a
ta93n <nideratakumin@gmail.com>
2020-03-30 00:30:46 +0900

    アカウント有効化機能の追加

--- db/seeds.rb
+++ db/seeds.rb
@@ -2,4 +2,6 @@ User.create!(name:  "ta93n",
              email: "nideratakumin@gmail.com",
              password:              "1234takumin",
              password_confirmation: "1234takumin",
-             admin: true)
+             admin: true,
+             activated: true,
+             activated_at: Time.zone.now)
