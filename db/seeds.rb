7dc75e0aa9bfe3709cdb007e2e81565dee0defba
ta93n <nideratakumin@gmail.com>
2020-05-08 04:58:47 +0900

    “seedの設定”

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,37 +1,15 @@
-User.create!(name:  "ta93n_01",
-             email: "nideratakumin@gmail.com",
+User.create!(name:  "たくみ",
+             email: ENV['TA93N_EMAIL_01'],
              password:              "password",
              password_confirmation: "password",
              admin: true,
              activated: true,
              activated_at: Time.zone.now)
 
-User.create!(name:  "ta93n_02",
-             email: "ta93n@icloud.com",
+User.create!(name:  "野村",
+             email: ENV['TA93N_EMAIL_02'],
              password:              "password",
              password_confirmation: "password",
              admin: false,
              activated: true,
              activated_at: Time.zone.now)
-
-User.create!(name:  "ta93n_03",
-            email: "nideratakumin@yahoo.co.jp",
-            password:              "password",
-            password_confirmation: "password",
-            admin: false,
-            activated: true,
-            activated_at: Time.zone.now)
-
-
-3.times do |n|
-name  = "sample_user-#{n+1}"
-email = "example-#{n+1}@sample.com"
-password = "password"
-User.create!(name: name,
-            email: email,
-            password:              password,
-            password_confirmation: password,
-            admin: false,
-            activated: true,
-            activated_at: Time.zone.now)
-end
