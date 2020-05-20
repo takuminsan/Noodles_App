e9144ba9293356d28b7df00e553d78b16d4a6e0d
ta93n <nideratakumin@gmail.com>
2020-05-21 01:28:10 +0900

    “seedsの編集”

--- db/seeds_relationships.rb
+++ db/seeds_relationships.rb
@@ -1,9 +1,9 @@
-10.times do |n|
+12.times do |n|
   users = User.all
   user  = users.find(n+1)
-  following = users[0..9]
+  following = users[0..11]
   following.shift(n+1)
-  followers = users[0..9]
+  followers = users[0..11]
   followers.shift(n+1)
   following.each { |followed| user.follow(followed) }
   followers.each { |follower| follower.follow(user) }
