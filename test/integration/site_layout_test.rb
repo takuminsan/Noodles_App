863dc99c74027a18f01336631f3f233177cef75c
ta93n <nideratakumin@gmail.com>
2020-06-08 17:09:45 +0900

    “少しレスポンシブ化”

--- test/integration/site_layout_test.rb
+++ test/integration/site_layout_test.rb
@@ -5,7 +5,7 @@ class SiteLayoutTest < ActionDispatch::IntegrationTest
   test "layout links" do
     get root_path
     assert_template 'static_pages/home'
-    assert_select "a[href=?]", root_path, count: 2
+    assert_select "a[href=?]", root_path, count: 3
     assert_select "a[href=?]", signup_path
     assert_select "a[href=?]", login_path
   end
