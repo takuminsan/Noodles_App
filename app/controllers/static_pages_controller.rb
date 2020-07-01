289fbaa8a7b778e9f2f539eb8a0a65ac460d6e5d
ta93n <nideratakumin@gmail.com>
2020-07-01 17:48:22 +0900

    “[change][clean]レイアウト変更,”

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,9 +1,9 @@
 class StaticPagesController < ApplicationController
   def home
-    if logged_in?
-      @feed_items = current_user.feed.paginate(page: params[:page], per_page: 10).recent
-    else
-      @feed_items = Post.all.paginate(page: params[:page], per_page: 10).recent
-    end
+    @feed_items = if logged_in?
+                    current_user.feed.paginate(page: params[:page], per_page: 10).recent
+                  else
+                    Post.all.paginate(page: params[:page], per_page: 10).recent
+                  end
   end
 end
