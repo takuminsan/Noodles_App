f8fe71ec45f46e77af51bc02ec756a78d58fd6ce
ta93n <nideratakumin@gmail.com>
2020-07-04 20:15:43 +0900

    “[clean]検索サイドバーの整理”

--- app/controllers/maps_controller.rb
+++ app/controllers/maps_controller.rb
@@ -1,11 +1,12 @@
 class MapsController < ApplicationController
-  before_action :logged_in_user, only: [:index, :map]
 
   def index
   end
 
   def map
-    if params[:posts] == "current_user"
+    if params[:posts] == "all_user"
+      @posts = Post.all.to_json.html_safe
+    elsif params[:posts] == "current_user"
       @posts = current_user.posts.to_json.html_safe
     elsif params[:posts] == "following"
       @posts = current_user.feed.to_json.html_safe
