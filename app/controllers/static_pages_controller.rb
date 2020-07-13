d6cc26362baf03d52c81b1c882ac0e45f4f33c25
ta93n <nideratakumin@gmail.com>
2020-07-13 15:30:43 +0900

    [add]ヘッダーに検索フォーム設置(完成)

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,9 +1,15 @@
 class StaticPagesController < ApplicationController
   def home
-    @feed_items = if logged_in?
-                    current_user.feed.paginate(page: params[:page], per_page: 12).recent
-                  else
-                    Post.all.paginate(page: params[:page], per_page: 12).recent
-                  end
+    @section_title = if logged_in?
+                       "自分とフォロー中のユーザーの投稿"
+                     else
+                       "全てのユーザーの投稿"
+                     end
+
+    @feed_items =    if logged_in?
+                       current_user.feed.paginate(page: params[:page], per_page: 12).recent
+                     else
+                       Post.all.paginate(page: params[:page], per_page: 12).recent
+                     end
   end
 end
