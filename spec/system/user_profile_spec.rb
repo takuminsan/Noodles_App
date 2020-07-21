cac7490b4cc7c406f36bdc635cc667d0dd67359a
ta93n <nideratakumin@gmail.com>
2020-07-21 21:09:11 +0900

    [add]user_profile_specの更新

--- spec/system/user_profile_spec.rb
+++ spec/system/user_profile_spec.rb
@@ -6,20 +6,12 @@ describe 'ユーザープロフィール', type: :system do
   end
 
   it 'ユーザープロフィールが表示されること' do
-    log_in_as(@user)
+    visit login_path
+    fill_in 'session_email', with: @user.email
+    fill_in 'session_password', with: @user.password
+    click_button 'ログイン'
     visit user_path(@user)
     expect(page).to have_content "投稿 (#{@user.posts.count})"
     expect(page).to have_content "食べたい！(#{@user.likes.count})"
   end
 end
-
-#
-#  test "profile display" do
-#    log_in_as(@user)
-#    get user_path(@user)
-#   assert_select 'a', text: "投稿 (#{@user.posts.count})"
-#  assert_select 'a', text: "食べたい！(#{@user.likes.count})"
-#    assert_match @user.posts.count.to_s, response.body
-#    assert_select 'div.pagination'
-#  end
-#end
