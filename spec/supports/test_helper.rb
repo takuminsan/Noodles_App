c227cabc93d613edea4dbcab31773595fce1d700
ta93n <nideratakumin@gmail.com>
2020-08-09 21:49:05 +0900

    [add]RSpec/システムテスト

--- spec/supports/test_helper.rb
+++ spec/supports/test_helper.rb
@@ -1,6 +1,6 @@
 def log_in_as(user)
-    visit login_path
-    fill_in 'session_email', with: user.email
-    fill_in 'session_password', with: 'password'
-    click_button 'ログイン'
+  visit login_path
+  fill_in 'session_email', with: user.email
+  fill_in 'session_password', with: 'password'
+  click_button 'ログイン'
 end
