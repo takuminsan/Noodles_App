a5cc109130caf73f8025f002ecf6141baf997706
ta93n <nideratakumin@gmail.com>
2020-08-11 17:37:18 +0900

    [add]RSpec/システムテスト

--- spec/system/like_spec.rb
+++ spec/system/like_spec.rb
@@ -11,8 +11,8 @@ describe 'いいね機能', type: :system do
     # ボタンを押す
     click_on '「食べたい！」'
     wait_for_ajax # spec/supports/test_helper.rb
-    expect(@user.likes.count).to eq 1
-    expect(@post.likes.count).to eq 1
+    expect(@user.reload.likes.count).to eq 1
+    expect(@post.reload.likes.count).to eq 1
     expect(page).to have_content 'あなたと0人が「食べたい！」'
     # 「食べたい！」ページに食べたい！ボタンを押した投稿が表示されること
     visit likes_user_path(@user)
@@ -20,7 +20,7 @@ describe 'いいね機能', type: :system do
     # ボタンを押して食べたい！を解除
     click_on 'あなたと0人が「食べたい！」'
     wait_for_ajax # spec/supports/test_helper.rb
-    expect(@user.likes.count).to eq 0
-    expect(@post.likes.count).to eq 0
+    expect(@user.reload.likes.count).to eq 0
+    expect(@post.reload.likes.count).to eq 0
   end
 end
