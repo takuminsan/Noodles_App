1cefd9c880a2126c69d9b77a7c645e9e0199294a
ta93n <nideratakumin@gmail.com>
2020-08-21 23:21:32 +0900

    [fix]ajaxを待つテストの修正

--- spec/system/following_spec.rb
+++ spec/system/following_spec.rb
@@ -11,10 +11,11 @@ describe 'フォロー', type: :system do
     # フォローするボタンを押す
     visit user_path(@other)
     click_button 'フォローする'
-    wait_for_ajax # spec/supports/test_helper.rb
-    expect(@user.following.count).to eq 1
-    expect(@other.followers.count).to eq 1
-    expect(page).to have_button 'フォロー解除'
+    wait_for_ajax do # spec/supports/test_helper.rb
+      expect(@user.following.count).to eq 1
+      expect(@other.followers.count).to eq 1
+      expect(page).to have_button 'フォロー解除'
+    end
     # followingページにフォロー中のユーザーが表示されること
     visit following_user_path(@user)
     expect(page).to have_link 'other'
